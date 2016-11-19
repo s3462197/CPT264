30fb49b925c87b8bfb59299809d7af3840ac091b
root <root@ip-172-31-22-110.ap-southeast-2.compute.internal>
2016-11-19 18:13:52 -0500

    updated user.pp with fred

--- user.pp
+++ user.pp
@@ -7,15 +7,32 @@ group { 'cars':
   			  ensure => 'present',
   			  gid    => '501',
      }
+	 
+group { 'trucks':
+  			  ensure => 'present',
+  			  gid    => '502',
+     }
 
 user { 'becca':
  			  ensure           => 'present',
       home             => '/home/becca',
-      comment           => 'becca someoneface',
+      comment           => 'Becca Someoneface',
       groups            => ['sysadmin', 'cars'],
       password         => '$1$RHVeyyNl$usSOMV5tcj75IZrRbfKe41',
       password_max_age => '99999',
       password_min_age => '0',
       shell            => '/bin/bash',
       uid              => '10012197',
+    }
+	
+user { 'fred':
+	  ensure           => 'present',
+      home             => '/home/fred',
+      comment           => 'Fred Guyface',
+      groups            => ['trucks', 'cars'],
+      password         => '$1$efsACfEN$cTFuuJUBnS0zsNQ/P0UaQ.',
+      password_max_age => '99999',
+      password_min_age => '0',
+      shell            => '/bin/csh',
+      uid              => '10022197',
     }
