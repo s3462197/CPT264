8c5823de655e570bfdb005ddabaf05a92c121d32
root <root@ip-172-31-22-110.ap-southeast-2.compute.internal>
2016-11-20 04:19:39 -0500

    final w. comments

--- packages.pp
+++ packages.pp
@@ -1,55 +1,66 @@
+#install openssl
 package { 'openssl':
   ensure => installed,
   name   => openssl,
 }
 
+#install openssh-server
 package { 'openssh-server':
   ensure  => installed,
   name    => openssh-server,
   require => Package['openssl'],
 }
 
+#install openssh-clients
 package { 'openssh-clients':
   ensure  => installed,
   name    => openssh-clients,
   require => Package['openssl'],
 }
 
+#install apache
 package { 'httpd':
   ensure  => installed,
   name    => httpd,  
 }
 
+#install mysql server (mariadb)
 package { 'mariadb-server':
   ensure  => installed,
   name    => mariadb-server,  
 }
 
+#install  mysql client
 package { 'mariadb':
   ensure  => installed,
   name    => mariadb,  
 }
 
+#install vnc server
 package { 'vncserver':
   ensure  => installed,
   name    => vnc-server,  
 }
 
+#install tmux
 package { 'tmux':
   ensure => installed,
   name   => tmux,
 }
 
+#install glibc developer package
 package { 'glibc-devel':
 	ensure	=>	installed,
 	name	=>	'glibc-devel.i686',
 }
 
+#install glibc
 package { 'glibc':
 	ensure	=>	installed,
 	name	=>	'glibc.i686',
 } 
 
+#install dia2code
 package { 'dia2code':
 	ensure	=>	installed,
 	name	=>	dia2code,
@@ -57,22 +68,26 @@ package { 'dia2code':
 	provider	=>	'rpm',
 }
 
+#install lynx
 package { 'lynx':
 	ensure	=>	installed,
 	name	=>	lynx,
 	install_options	=>	[ { '--enablerepo'	=>	'rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional'} ],
 }
 
+#install gcc
 package { 'gcc':
   ensure => installed,
   name   => gcc,
 }
 
+#install gdb
 package { 'gdb':
   ensure => installed,
   name   => gdb,
 }
 
+#install epel
 package { 'epel-release':
 	ensure	=>	installed,
 	name	=>	epel-release,
@@ -80,22 +95,26 @@ package { 'epel-release':
 	provider	=>	'rpm',
 }
 
+#install cgdb
 package { 'cgdb':
 	require => Package['epel-release'],
 	ensure	=>	installed,
 	name	=>	cgdb
 }
 
+#install vim
 package { 'vim':
   ensure => installed,
   name   => vim,
 }
 
+#install emacs
 package { 'emacs':
   ensure => installed,
   name   => emacs,
 }
 
+#install sshfs
 package { 'sshfs':
   ensure => installed,
   name   => sshfs,
