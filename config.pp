d4ca90fb086c09f692e10806aee8f0f6114c7c8e
root <root@ip-172-31-22-110.ap-southeast-2.compute.internal>
2016-11-20 03:24:18 -0500

    added config.pp

---
+++ config.pp
@@ -0,0 +1,28 @@
+#disable ssh root access
+file_line { 'sshRootDisable':
+	ensure 		=> 	present,
+	path   		=> 	'/etc/ssh/sshd_config',
+	line   		=> 	'PermitRootLogin no',
+	match  		=> 	'^PermitRootLogin',	
+}
+
+#move document root
+file_line { 'documentRoot':
+	ensure	=> present,
+	path	=>	'/etc/httpd/conf/httpd.conf',
+	line	=>	'DocumentRoot "/var/www/s3462197"',
+	match	=>	'^DocumentRoot',
+}
+
+#grant becca sudo access 
+file_line { 'sudoers':
+	ensure	=> present,
+	path	=>	'/etc/sudoers',
+	line	=>	'becca	ALL=(ALL)	ALL',	
+}
+
+#Ensure /usr/local/bin is in everyone's path on the system
+file { '/etc/profile.d':
+	ensure =>	present,
+	source =>	'/usr/local/bin',
+}
